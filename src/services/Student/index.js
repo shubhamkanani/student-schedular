596f4b04c22b6299eab4d79e024f3e606f4794d1
shubhamkanani <shubhamkanani605@gmail.com>
2020-09-29 00:03:03 +0530

    new code

--- src/services/Student/index.js
+++ src/services/Student/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const getStudentListById = (TeacherId) =>{
-    return axios.get(`${process.env.REACT_APP_BASE_API_URL}/students/search/findByTeacherId?id=${TeacherId}`)
+    return axios.get(`http://tower.watu.fi:8080/students/search/findByTeacherId?id=${TeacherId}`)
         .then(res =>{
             console.log(res.data);
             return res.data;
@@ -8,21 +8,21 @@ export const getStudentListById = (TeacherId) =>{
 }
 
 export const getStudentList = (page,size) =>{
-    return axios.get(`${process.env.REACT_APP_BASE_API_URL}/students?page=${page}&size=${size}`)
+    return axios.get(`http://tower.watu.fi:8080/students?page=${page}&size=${size}`)
         .then(res =>{
             return res.data;
         })
 }
 
 export const getStudentListByFirstName = (name) =>{
-    return axios.get(`${process.env.REACT_APP_BASE_API_URL}/students/search/findByFirstName?name=${name}`)
+    return axios.get(`http://tower.watu.fi:8080/students/search/findByFirstName?name=${name}`)
         .then(res =>{
             return res.data;
         })
 }
 
 export const getStudentListByLastName = (name) =>{
-    return axios.get(`${process.env.REACT_APP_BASE_API_URL}/students/search/findByLastName?name=${name}`)
+    return axios.get(`http://tower.watu.fi:8080/students/search/findByLastName?name=${name}`)
         .then(res =>{
             return res.data;
         })
--- src/services/Teacher/index.js
+++ src/services/Teacher/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 export const getTeacherList = (page,size) =>{
-    return axios.get(`${process.env.REACT_APP_BASE_API_URL}/teachers?page=${page}&size=${size}`)
+    return axios.get(`http://tower.watu.fi:8080/teachers?page=${page}&size=${size}`)
         .then(res =>{
             return res.data;
         })
 }
 
 export const getTeacherListByFirstName = (name) =>{
-    return axios.get(`${process.env.REACT_APP_BASE_API_URL}/teachers/search/findByFirstName?name=${name}`)
+    return axios.get(`http://tower.watu.fi:8080/teachers/search/findByFirstName?name=${name}`)
         .then(res =>{
             console.log("env : ", process.env.REACT_APP_BASE_API_URL)
             return res.data;
@@ -15,7 +15,7 @@ export const getTeacherListByFirstName = (name) =>{
 }
 
 export const getTeacherListByLastName = (name) =>{
-    return axios.get(`${process.env.REACT_APP_BASE_API_URL}/teachers/search/findByLastName?name=${name}`)
+    return axios.get(`http://tower.watu.fi:8080/teachers/search/findByLastName?name=${name}`)
         .then(res =>{
             return res.data;
         })
